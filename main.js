ce6f7f48ba7725e19940e1911e052fa486f141bf
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2023-09-22 16:10:43 +0300

    ToDO

---
+++ main.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.text');
+const addBut = document.querySelector('.add');
+const ul = document.querySelector('.tasks')
+
+addBut.addEventListener('click', () => {
+    if (input.value) {
+        let div = document.createElement('div');
+        div.classList.add('item');
+        div.innerHTML = input.value;
+        let butt = document.createElement('button');
+        butt.classList.add('currentButt');
+        butt.innerHTML = 'DELETE';
+        div.append(butt);
+        ul.appendChild(div);
+        input.value = '';
+
+        butt.addEventListener('click', () => {
+            ul.removeChild(div);
+
+        })
+    }
+})
